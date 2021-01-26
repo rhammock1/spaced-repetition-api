7a9100e6771b6fee17642ebb10bd7e69ad6f7c00
rhammock1 <rhamm1027@gmail.com>
2021-01-26 10:31:34 -0500

    added methods for doublelinkedlist class

--- src/language/linked-list.js
+++ src/language/linked-list.js
@@ -12,4 +12,84 @@ class DoublyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  insertFirst(value) {
+    const node = new _Node(value);
+    if (!this.length) {
+      this.head = node;
+      this.tail = node;
+      this.length++;
+      return this.head;
+    } else {
+      this.head.next = node;
+      node.prev = this.head;
+      this.head = node;
+      this.length++;
+      return this.head;
+    }
+  }
+
+  insertLast(value) {
+    if (this.head === null) {
+      this.insertFirst(value);
+    } else {
+      const node = new _Node(value);
+      this.tail.prev = node;
+      node.next = this.tail;
+      this.tail = node;
+      this.length++;
+      return this.tail;
+    }
+  }
+
+  find(value) {
+    if (!this.head) {
+      return null;
+    }
+    // This method searches from both ends at the same time
+    let headNode = this.head;
+    let tailNode = this.tail;
+    let foundNode;
+    while (headNode !== tailNode.next) {
+      if(headNode.value === value) {
+        foundNode = headNode;
+      } else if (tailNode.value === value) {
+        foundNode = tailNode;
+      }
+      headNode = headNode.prev;
+      tailNode = tailNode.next;
+    }
+    return foundNode;
+  }
+
+  remove(value) {
+    if (!this.head) {
+      return null;
+    }
+    if (this.head.value === value) {
+      this.head = this.head.prev;
+      this.length--
+      return;
+    }
+    const nodeToRemove = this.find(value);
+    let prevNode = nodeToRemove.prev;
+    let nextNode = nodeToRemove.next;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+    this.length--;
+  }
+
+  display() {
+    const values = [];
+    let length = this.length;
+    let tempNode = this.head;
+    while (length) {
+      values.push(tempNode.value)
+      tempNode = tempNode.prev;
+      length--;
+    }
+    return values;
+  }
 }
+
+module.exports = DoublyLinkedList;
