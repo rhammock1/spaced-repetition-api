2e7c4da3c070dd3cd24076124299fa074448ec46
rhammock1 <rhamm1027@gmail.com>
2021-01-28 10:01:38 -0500

    small changes

--- src/server.js
+++ src/server.js
@@ -2,16 +2,15 @@ require('dotenv').config()
 
 const knex = require('knex')
 const app = require('./app')
-const { PORT } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: process.env.DATABASE_URL,
-  ssl: 'required',
+  connection: DATABASE_URL,
 })
 
 app.set('db', db)
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening at Port:${PORT}`)
 })
